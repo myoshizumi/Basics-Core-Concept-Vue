984f23859d7fba7c0f544a5f3fa91cec77c63062
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-01-28 15:43:59 +0900

    Dynamic Styling with Inline Styles

--- app.js
+++ app.js
@@ -1,67 +1,23 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			counter: 0,
-			name: "",
-			lastName: "",
-			// fullname: "",
+			boxASelected: false,
+			boxBSelected: false,
+			boxCSelected: false,
 		};
 	},
-	watch: {
-		counter(value) {
-			if (value > 50) {
-				const that = this;
-				setTimeout(() => {
-					that.counter = 0;
-				}, 2000);
-			}
-		},
-		// name(value) {
-		// 	if (value === "") {
-		// 		this.fullname = "";
-		// 	} else {
-		// 		this.fullname = value + " " + this.lastName;
-		// 	}
-		// },
-		// lastName(value) {
-		// 	if (value === "") {
-		// 		this.fullname = "";
-		// 	} else {
-		// 		this.fullname = this.name + " " + value;
-		// 	}
-		// },
-	},
-	computed: {
-		fullname() {
-			console.log("Runnning again...");
-			if (this.name === "" || this.lastName === "") {
-				return "";
-			}
-			return this.name + " " + this.lastName;
-		},
-	},
+
 	methods: {
-		outputFullname() {
-			if (this.name === "") {
-				return "";
+		boxSelected(box) {
+			if (box === "A") {
+				this.boxASelected = true;
+			} else if (box === "B") {
+				this.boxBSelected = true;
+			} else if (box === "C") {
+				this.boxCSelected = true;
 			}
-			return this.name + " " + "test";
-		},
-		setName(event) {
-			this.name = event.target.value;
-		},
-		add(num) {
-			this.counter = this.counter + num;
-		},
-		reduce(num) {
-			this.counter = this.counter - num;
-			// this.counter--;
-		},
-		resetInput() {
-			this.name = "";
-			this.lastName = "";
 		},
 	},
 });
 
-app.mount('#events');
+app.mount("#styling");
