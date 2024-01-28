3162d8ed8df5fc364b79fa4eb7892f760add6cbe
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-01-28 14:03:15 +0900

    Data Binding + Event Binding = Two-Way Binding

--- app.js
+++ app.js
@@ -3,25 +3,21 @@ const app = Vue.createApp({
 		return {
 			counter: 0,
 			name: "",
-			confirmedName: "",
 		};
 	},
 	methods: {
-		confirmInput() {
-			this.confirmedName = this.name;
-		},
-		submitForm(e) {
-			// e.preventDefault();
-			alert("Sumitted!");
-		},
 		setName(event, lastName) {
-			this.name = event.target.value + lastName;
+			this.name = event.target.value + " " + lastName;
 		},
 		add(num) {
 			this.counter = this.counter + num;
 		},
 		reduce(num) {
 			this.counter = this.counter - num;
+			// this.counter--;
+		},
+		resetInput() {
+			this.name = "";
 		},
 	},
 });
