dde74840fcf8d5a4e3a7639474419eb937864957
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-01-28 12:52:23 +0900

    Understanding Event Binding

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const app = Vue.createApp({
-	data() {
-		return {
-			courseGoalA: "Finish the course and learn Vue!",
-			courseGoalB: "Master Vue and build amazing apps!",
-			vueLink: "https://vuejs.org/",
-		};
-	},
-	methods: {
-		outputGoal() {
-			const randomNumber = Math.random();
-
-			if (randomNumber < 0.5) {
-				return this.courseGoalA;
-			} else {
-				return this.courseGoalB;
-			}
-		},
-	},
+  data() {
+    return {
+      counter: 0,
+    };
+  },
 });
 
-app.mount("#user-goal");
+app.mount('#events');
